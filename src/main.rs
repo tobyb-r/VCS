2faf71641c5d30f3dfb473b927b38bcc14a5d2d2
txb349 <txb349@student.bham.ac.uk>
2025-02-23 16:55:48 +0000

    fleshed out plan for interface more

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@ use std::env;
 fn main() {
     let args = env::args().collect::<Vec<String>>();
 
+    if args.len() == 1 {
+        println!("Status");
+    }
+
     let command = &args[1];
 
     match command.as_str() {
+        "diff" => {
+            println!("Difference of two commits");
+        }
         "init" => {
             println!("Initializing repository");
         }
@@ -19,15 +26,33 @@ fn main() {
         "branch" => {
             println!("Branching");
             // -c --checkout <commit or branch name>
+            // -r --reset <commit or branch name>
             // -n --new <name>
             // -d --delete <name>
+        }
+        "remote" => {
+            // change or update remote repository
+            println!("Updating remote");
+            // -u --update <url>
+            // -r --remove
+        }
+        "clone" => {
+            // clone from remote repository
+            println!("Cloning remote");
+            // <url>
+        }
+        "push" => {
+            println!("Pushing changes to remote");
+        }
+        "pull" => {
+            println!("Pulling changes from remote");
         }
         "merge" => {
             println!("Merging branches");
             // <commit or branch name 1> <commit or branch name 2>
         }
-        "status" => {
-            println!("Status");
+        "help" => {
+            println!("Help");
         }
         other => {
             panic!("Unkown command {}", other);
